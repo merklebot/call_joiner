43a36df60b55c88ca3f7649cc5813062a09f8e2b
Alisher A. Khassanov <a.khssnv@gmail.com>
2021-10-15 15:09:55 +0300

    Ignore login failure

--- call_joiner/browser.py
+++ call_joiner/browser.py
@@ -2,7 +2,7 @@ import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import ElementNotInteractableException, TimeoutException
 from selenium.webdriver import Remote as WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -24,24 +24,27 @@ class GoogleMeet:
         self.webdriver.switch_to.window(self.webdriver.window_handles[0])
 
     def join_call(self, url: str):
-        self.webdriver.get("https://accounts.google.com/ServiceLogin")
-        email_input = WebDriverWait(self.webdriver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//input[@type='email']"))
-        )
-        email_input.send_keys(settings.GOOGLE_MEET_CREDS.EMAIL)
-        next_button = self.webdriver.find_element_by_xpath(
-            "//*[@id='identifierNext']/div/button"
-        )
-        next_button.click()
-        time.sleep(2)
-        password_input = WebDriverWait(self.webdriver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//input[@type='password']"))
-        )
-        password_input.send_keys(settings.GOOGLE_MEET_CREDS.PASSWORD)
-        next_button = self.webdriver.find_element_by_xpath(
-            "//*[@id='passwordNext']/div/button"
-        )
-        next_button.click()
+        try:
+            self.webdriver.get("https://accounts.google.com/ServiceLogin")
+            email_input = WebDriverWait(self.webdriver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//input[@type='email']"))
+            )
+            email_input.send_keys(settings.GOOGLE_MEET_CREDS.EMAIL)
+            next_button = self.webdriver.find_element_by_xpath(
+                "//*[@id='identifierNext']/div/button"
+            )
+            next_button.click()
+            time.sleep(2)
+            password_input = WebDriverWait(self.webdriver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//input[@type='password']"))
+            )
+            password_input.send_keys(settings.GOOGLE_MEET_CREDS.PASSWORD)
+            next_button = self.webdriver.find_element_by_xpath(
+                "//*[@id='passwordNext']/div/button"
+            )
+            next_button.click()
+        except TimeoutException:
+            pass
 
         t1 = time.time()
         while time.time() - t1 < 60:
